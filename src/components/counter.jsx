fade93c9db1ae99196a8fbf9d485043bff45aa3e
moaz <mohsin.afridi91@gmail.com>
2020-03-12 00:19:47 +0500

    update counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -35,6 +35,7 @@ class Counter extends Component {
                {/* <img src={this.state.imageUrl} alt=""/> */}
             {/* <span style={{ fontSize:30 }} className="badge badge-primary m-2">{ this.formatCount() }</span>  */}
             
+            
             <span className={ this.getBadgeClasses()}>{ this.formatCount() }</span> 
             <button onClick={ () => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button> 
             <button 
